d96904c732dc1568f81bf5e84520353d44c6564c
Jake Brown <JakeBrown@jbrownjake.com>
2023-06-08 10:14:01 -0400

    adjustments more+

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,35 +1,43 @@
 import './Projects.css';
 
-const Projects=()=>{
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-    return(
-        <div className='projects-div'>
-        <h1>Project-Component</h1>
-        <h2>Projects</h2>
-        <p>need to add area here for projects</p>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        </div>
-    );
+const projectsData = [
+  {
+    id: 1,
+    name: 'Project 1',
+    description: 'This is the description of Project 1.',
+    demoLink: 'https://project1demo.com'
+  },
+  {
+    id: 2,
+    name: 'Project 2',
+    description: 'This is the description of Project 2.',
+    demoLink: 'https://project2demo.com'
+  },
+  // Add more projects as needed
+];
+
+const Projects = () => {
+  return (
+    <div className='projects-container'>
+      <h2>Projects</h2>
+      <div className='projects-grid'>
+        {projectsData.map((project) => (
+          <div className='project-card' key={project.id}>
+            <h3>{project.name}</h3>
+            <p>{project.description}</p>
+            <a href={project.demoLink} target='_blank' rel='noopener noreferrer'>
+              Demo
+            </a>
+          </div>
+        ))}
+      </div>
+      <Link to='/'>Go Back</Link>
+    </div>
+  );
 };
 
 export default Projects;
+
