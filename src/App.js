1e90859f7eecf3c2c4058350b36dc1832cbb52a3
Jake Brown <JakeBrown@jbrownjake.com>
2023-06-08 11:44:48 -0400

    adjustments

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
-import React from "react";
-import Navbar from "./components/Nav/Navbar";
-import About from "./Pages/About";
-import Contact from "./Pages/Contact";
-import Resume from "./Pages/Resume";
-import Projects from "./Pages/Projects";
-import Home from "./Pages/Home";
-import {Route, Routes} from "react-router-dom";
-import Footer from "./components/Footer/Footer";
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Navbar from './components/Nav/Navbar';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Resume from './Pages/Resume';
+import Projects from './Pages/Projects';
+import Home from './Pages/Home';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
-
 function App() {
-
   return (
     <div className="main-div">
       <Navbar />
       <div className="container-div">
         {/* <Sidebar /> */}
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/contact" element={<Contact/>} />
-          <Route path="/resume" element={<Resume/>} />
-          <Route path="/projects" element={<Projects/>} />
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/projects" element={<Projects />} />
         </Routes>
       </div>
       <Footer />
-      </div>
+    </div>
   );
 }
 
 export default App;
-
